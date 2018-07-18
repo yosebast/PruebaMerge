30c9319478a17832f6422520d7dbe08ed41b0fbd
yosebast <ik_sbtian@yahoo.es>
2018-07-18 21:47:56 +0200

    solventado el merge

--- PruebaMerge/src/main/java/com/example/service/ServicePrueba1.java
+++ PruebaMerge/src/main/java/com/example/service/ServicePrueba1.java
@@ -1,5 +1,12 @@
 package com.example.service;
 
 public class ServicePrueba1 {
+	
+	public String getPrueba1() {
+		return "prueba1 desde sebast";
+	}
 
+	public String getPrueba1String() {
+		return "prueba1 desde vladimir";
+	}
 }
